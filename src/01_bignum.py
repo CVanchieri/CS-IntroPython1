1f29f4b96a6a2814ba1a70c38e289c57602a87be
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2020-07-08 14:00:06 -0700

    Update 01_bignum.py

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,9 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-# YOUR CODE HERE
+num1 = 2**65536
+num2 = pow(2, 65536)
+print("2**65536")
+print(num1)
+print("pow(2,65536)")
+print(num2)
