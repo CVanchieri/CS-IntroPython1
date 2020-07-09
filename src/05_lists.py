fe936c55f9ac1a8e307aa1dc9997a92728213ca0
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2020-07-08 20:38:26 -0700

    Update 05_lists.py

--- src/05_lists.py
+++ src/05_lists.py
@@ -8,22 +8,41 @@ y = [8, 9, 10]
 
 # Change x so that it is [1, 2, 3, 4]
 # YOUR CODE HERE
-print(x)
+print("change x so that it is [1, 2, 3, 4]")
+x.append(4)
+print("x.append(4)")
+print(f"x is now = {x}")
 
 # Using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]
 # YOUR CODE HERE
-print(x)
+print("using y, change x so that it is [1, 2, 3, 4, 8, 9, 10]")
+x.extend(y)
+print("x.append([y])")
+print(f"x is now = {x}")
 
 # Change x so that it is [1, 2, 3, 4, 9, 10]
 # YOUR CODE HERE
-print(x)
+print("change x so that it is [1, 2, 3, 4, 9, 10]")
+x.remove(8)
+print("x.remove(8)")
+print(f"x is now = {x}")
 
 # Change x so that it is [1, 2, 3, 4, 9, 99, 10]
 # YOUR CODE HERE
-print(x)
+x.insert(5, 99)
+print("x.insert(5, 99)")
+print(f"x is now = {x}")
 
 # Print the length of list x
-# YOUR CODE HERE
+length = len(x)
+print("length(x)")
+print(f"list x length is = {length}")
 
 # Print all the values in x multiplied by 1000
-# YOUR CODE HERE
+x2 = []
+for i in x:
+    x2.append(i * 1000)
+print("""x2 = []
+for i in x:
+    x2.append(i * 1000)""")
+print(x2)
