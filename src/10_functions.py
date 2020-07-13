0e7394459a12991abab9101457c0332e8582de55
Charles Vanchieri <32582544+CVanchieri@users.noreply.github.com>
2020-07-13 15:15:50 -0700

    Update 10_functions.py

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,11 +1,18 @@
+# Read a number from the keyboard
+num = input("Enter a number: ") # take an unput from the user.
+num = int(num) # set input as int.
 # Write a function is_even that will return true if the passed-in number is even.
-
-# YOUR CODE HERE
+def print_num(num): # function 
+    if num % 2 == 0: # if remainder is 0 its true.
+        print("true!")
+    else:
+        print("false!")
+    print(num)
+print_num(num) # print result.
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
-
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 def print_num(num):
     if num % 2 == 0:
@@ -13,7 +20,5 @@ def print_num(num):
     else:
         print("Odd!")
     print(num)
-num = input("Enter a number: ")
-num = int(num)
 print_num(num)
 
